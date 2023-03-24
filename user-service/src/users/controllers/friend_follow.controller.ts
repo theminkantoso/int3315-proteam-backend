07a885a48380268c11fff532e30809a98fca4a05
tuyenshin2004 <tuyenshin2004@gmail.com>
2023-03-24 21:23:23 +0700

    unfriend
    unfriend

--- user-service/src/users/controllers/friend_follow.controller.ts
+++ user-service/src/users/controllers/friend_follow.controller.ts
@@ -1,9 +1,10 @@
+import { UnfriendDto } from './../dtos/friend_follow/unfriend.dto';
 import { AcceptFriendDto } from './../dtos/friend_follow/accept_friend.dto';
 import { FriendFollowDto } from './../dtos/friend_follow/friend_follow.dto';
 import { FriendRequestDto } from './../dtos/friend_follow/friend_request.dto';
 import { FriendFollowService } from './../services/friend_follow.service';
 import { classToPlain, instanceToPlain } from 'class-transformer';
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBasicAuth,
@@ -23,7 +24,7 @@ export class FriendFollowController {
   constructor(private readonly friendFollowService: FriendFollowService) {}
 
   @ApiOperation({ summary: 'Send friend request (jwt required)' })
-  @ApiResponse({ status: 200, description: 'Current Friend Follow info' })
+  @ApiResponse({ status: 200, description: 'message' })
   @ApiNotFoundResponse({ status: 404, description: 'User not found' })
   @ApiBadRequestResponse({ status: 400, description: '' })
   // @UseGuards(JwtAuthGuard)
@@ -34,7 +35,7 @@ export class FriendFollowController {
   }
 
   @ApiOperation({ summary: 'Accept friend request (jwt required)'})
-  @ApiResponse({ status: 200, description: 'Current Friend Follow info' })
+  @ApiResponse({ status: 200, description: 'message' })
   @ApiNotFoundResponse({ status: 404, description: 'User not found' })
   @ApiBadRequestResponse({ status: 400, description: '' })
   // @UseGuards(JwtAuthGuard)
@@ -43,5 +44,27 @@ export class FriendFollowController {
     let id = 3;
     return instanceToPlain(this.friendFollowService.updateFriendFollow(id, acceptFriend));
   }
+
+  @ApiOperation({ summary: 'Unfriend request (jwt required)'})
+  @ApiResponse({ status: 200, description: 'message' })
+  @ApiNotFoundResponse({ status: 404, description: 'User not found' })
+  @ApiBadRequestResponse({ status: 400, description: '' })
+  // @UseGuards(JwtAuthGuard)
+  @Delete('unfriend')
+  async unfriend( @Body() unfriend: UnfriendDto) {
+    let id = 1;
+    return instanceToPlain(this.friendFollowService.unfriend(id, unfriend));
+  }
+
+  // @ApiOperation({ summary: 'Get list friend request (jwt required)' })
+  // @ApiResponse({ status: 200, description: 'List friend request' })
+  // @ApiNotFoundResponse({ status: 404, description: 'User not found' })
+  // @ApiBadRequestResponse({ status: 400, description: '' })
+  // // @UseGuards(JwtAuthGuard)
+  // @Get('list-friend-request')
+  // async listFriendRequest() {
+  //   let id = 1;
+  //   return instanceToPlain(this.friendFollowService.saveFriendFollow(id, friendRequestDto));
+  // }
 
 }
