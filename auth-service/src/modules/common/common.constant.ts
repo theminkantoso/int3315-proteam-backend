5c39b5d2f182ca1622b0b1040f6ad6e02da50258
duclv4 <duclv@tokyotechlab.com>
2023-03-25 00:21:30 +0700

    fix auth service

--- auth-service/src/modules/common/common.constant.ts
+++
@@ -1,4 +0,0 @@
-export enum WITH_INACTIVE_OPTION {
-    YES = 1,
-    NO = 0,
-}
--- auth-service/src/modules/file/file.constants.ts
+++
@@ -1,4 +0,0 @@
-export const PRESIGNED_URL_EXPIRED_IN = 500; // 5 minutes
-
-// max size upload file 10 * 1024 * 1024 = 10485760
-export const MAX_SIZE_FILE = 10485760;
