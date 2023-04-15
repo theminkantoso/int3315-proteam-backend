098330579a0095b81c5b10c15a872e8e41dfa907
duclv4 <duclv@tokyotechlab.com>
2023-04-15 10:47:26 +0700

    fix env error

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { createProxyMiddleware } from 'http-proxy-middleware';
@@ -10,12 +12,32 @@ async function bootstrap() {
   // app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
 
-  const AUTH_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.AUTH_SERVICE : process.env.AUTH_SERVICE_TUYEN;
-  const USER_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.USER_SERVICE : process.env.USER_SERVICE_TUYEN;
-  const POST_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.POST_SERVICE : process.env.POST_SERVICE_TUYEN;
-  const CHAT_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.CHAT_SERVICE : process.env.CHAT_SERVICE_TUYEN;
-  const NOTI_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.NOTI_SERVICE : process.env.NOTI_SERVICE_TUYEN;
-  const STATS_SERVICE_URL = process.env.ENVIRONMENT == 'docker'? process.env.STATS_SERVICE : process.env.STATS_SERVICE_TUYEN;
+  const AUTH_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.AUTH_SERVICE
+      : process.env.AUTH_SERVICE_TUYEN;
+  const USER_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.USER_SERVICE
+      : process.env.USER_SERVICE_TUYEN;
+  const POST_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.POST_SERVICE
+      : process.env.POST_SERVICE_TUYEN;
+  const CHAT_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.CHAT_SERVICE
+      : process.env.CHAT_SERVICE_TUYEN;
+  const NOTI_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.NOTI_SERVICE
+      : process.env.NOTI_SERVICE_TUYEN;
+  const STATS_SERVICE_URL =
+    process.env.ENVIRONMENT == 'docker'
+      ? process.env.STATS_SERVICE
+      : process.env.STATS_SERVICE_TUYEN;
+
+  console.log('first', AUTH_SERVICE_URL);
 
   // Proxy endpoints
   app.use(
